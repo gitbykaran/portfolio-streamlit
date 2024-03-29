f2a87098531b3f6fbdc43cd685add6e2f2fcb823
karanongithub <karandeepsingh3451@gmail.com>
2024-03-29 18:54:03 +0530

    reached till projects section

--- app.py
+++ app.py
@@ -15,3 +15,47 @@ st.info('''
 - As a dedicated Data Scientist, I bring a blend of technical expertise and strong soft skills to the table. Proficient in Python, R, and SQL, I excel in extracting, cleaning, and analyzing complex datasets to derive actionable insights.
 - Overall, I am a results-driven professional who is not only capable of delivering impactful data-driven solutions but also of fostering a collaborative and inclusive work environment conducive to innovation and growth.
 ''')
+
+
+def txt(a, b):
+    col1, col2 = st.columns([4, 1])
+    with col1:
+        st.markdown(a)
+    with col2:
+        st.markdown(b)
+
+
+st.markdown('''
+## Education
+''')
+
+txt('**Bachelor of Technology** (Information Technology), *CEC-CGC*, Landran',
+    '2021-2025')
+st.markdown('''
+- GPA: `7.73`
+- Coursework in `DSA`,`DBMS`,`ML`,`OOPS`, `BD` and statistical methods essential for data science proficiency.
+- Possess a strong foundation and practical skills in *data manipulation*, *analysis*, and *visualization*, poised to contribute effectively to the dynamic field of data science.
+- Commitment to Open Source Platforms like *Github*, *Leetcode*, *Hackerank* and *Kaggle*
+''', unsafe_allow_html=True)
+
+st.markdown('''
+## Work Experience
+''')
+
+txt('**Python Training**, ICS Group, *Chandigarh Engineering College*, Landran',
+    'July 2023')
+
+st.markdown('''
+- Employed Python for machine learning model training, leveraging libraries such as TensorFlow and PyTorch to implement deep learning architectures and optimize model performance
+- Utilized NumPy and Pandas for *data preprocessing*, *feature engineering*, and *manipulation tasks*, ensuring the input data is appropriately structured for effective model training.
+''', unsafe_allow_html=True)
+
+txt('**Data Science Internship**, Plasmid Innovation, Bengaluru,Karnataka',
+    'Feb-March 2024')
+
+st.markdown('''
+- Mastered foundational data science concepts like *statistical hypothesis testing*, *feature scaling*, and *dimensionality reduction* in the initial month, establishing a strong theoretical framework for subsequent project work.
+- Implemented advanced machine learning algorithms, including ensemble methods like *Random Forests* and *Gradient Boosting*, during the second month's projects, demonstrating expertise in *model selection* and optimization for diverse data sets.            
+''')
+
+st.markdown('## Projects')
