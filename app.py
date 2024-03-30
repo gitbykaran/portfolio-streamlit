53d12268eded151a12fdf20c20bb38e0f0d729a1
karanongithub <karandeepsingh3451@gmail.com>
2024-03-30 11:45:36 +0530

    removed toggle

--- app.py
+++ app.py
@@ -31,8 +31,6 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
   <a class="navbar-brand" href="#" target="_blank">Karandeep Singh</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
